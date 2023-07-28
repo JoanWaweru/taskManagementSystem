41944821f09443caf3f09a68d12dba31b3986a10
Joan Waweru <joan.waweru@strathmore.edu>
2023-07-28 10:59:48 +0300

    login and. registration template done

--- main.py
+++ main.py
@@ -1,16 +1,83 @@
-# This is a sample Python script.
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import sqlite3
+import hashlib
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = Flask(__name__)
+app.secret_key = 'undersecretary23'  # This is for session management
 
+def get_db_connection():
+    conn = sqlite3.connect('users.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def create_table():
+    conn = get_db_connection()
+    conn.execute('''
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL,
+            password TEXT NOT NULL
+        )
+    ''')
+    conn.commit()
+    conn.close()
 
+@app.route('/')
+def index():
+    return render_template('index.html')
 
-# Press the green button in the gutter to run the script.
+@app.route('/register', methods=['POST'])
+def register():
+    data = request.form
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'error': 'Username and password are required.'}), 400
+
+    conn = get_db_connection()
+    password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+    # Check if the username is already taken
+    existing_user = conn.execute('SELECT * FROM users WHERE username = ?', (username,)).fetchone()
+    if existing_user:
+        return jsonify({'error': 'Username already taken.'}), 400
+
+    # Insert the new user into the database
+    conn.execute('INSERT INTO users (username, password) VALUES (?, ?)', (username, password_hash))
+    conn.commit()
+    conn.close()
+    return jsonify({'message': 'Registration successful.'}), 201
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.form
+    username = data.get('username')
+    password = data.get('password')
+
+    if not username or not password:
+        return jsonify({'error': 'Username and password are required.'}), 400
+
+    conn = get_db_connection()
+    password_hash = hashlib.sha256(password.encode()).hexdigest()
+
+    # Check if the username and password match
+    user = conn.execute('SELECT * FROM users WHERE username = ? AND password = ?', (username, password_hash)).fetchone()
+    conn.close()
+
+    if not user:
+        return jsonify({'error': 'Invalid username or password.'}), 401
+
+    # Store the user's id in the session for later use (e.g., for authentication)
+    session['user_id'] = user['id']
+    return jsonify({'message': 'Login successful.'}), 200
+
+@app.route('/logout')
+def logout():
+    # Clear the user's session data
+    session.clear()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    create_table()
+    app.run(debug=True)
